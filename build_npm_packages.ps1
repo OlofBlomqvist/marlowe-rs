29fc5a6deda81b98f69c0d0e827cfb3b6376f47b
Olof Blomqvist <olof@twnet.se>
2023-08-03 22:12:58 +0200

    update nodejs example

--- build_npm_packages.ps1
+++ build_npm_packages.ps1
@@ -11,6 +11,7 @@ param(
 $nodejs_dir = "pkg_njs"
 $bundle_dir = "pkg_bundle"
 $web_dir = "pkg_web"
+$latest_dir = "pkg_latest"
 
 if($skipBuild -ne $true) {
 
@@ -24,7 +25,9 @@ if($skipBuild -ne $true) {
     if(Test-Path $web_dir) {
         Remove-Item $web_dir/ -Recurse -Force -ErrorAction Stop
     }
-
+    if(Test-Path $latest_dir) {
+        Remove-Item $latest_dir/ -Recurse -Force -ErrorAction Stop
+    }
     # Build for NodeJS
     wasm-pack build --all-features --release --target nodejs --out-dir $nodejs_dir/pkg
     $j = Get-Content $nodejs_dir/pkg/package.json |convertfrom-json
@@ -71,22 +74,38 @@ if($skipBuild -ne $true) {
     $bundlev = $j.version
     #wasm-pack pack $bundle_dir
 
+    # Build latest
+    wasm-pack build --all-features --release --target nodejs --out-dir $latest_dir/pkg
+    $j = Get-Content $latest_dir/pkg/package.json |convertfrom-json
+    if($null -ne $version -and $version.Length -gt 0) {
+        $j.version = $version
+    } else {
+        $j.version = $j.version
+    }   
+    $j | convertto-json | out-file .\$latest_dir/pkg/package.json
+    
+    $latestlev = $j.version
+    #wasm-pack pack $bundle_dir
+
     Write-Host -ForegroundColor Green "---> $njsv"
     Write-Host -ForegroundColor Green "---> $webv"
     Write-Host -ForegroundColor Green "---> $bundlev"
+    Write-Host -ForegroundColor Green "---> $latestlev"
 
 } else {
     Write-Warning "Build skipped"
 }
 
 if($publish) {
-    write-host -ForegroundColor Green "Publishing web... $web_dir $webv"
-    wasm-pack publish -t web --tag web $web_dir
-    write-host -ForegroundColor Green "Publishing nodejs... $nodejs_dir $njsv"
-    wasm-pack publish -t nodejs --tag nodejs   $nodejs_dir
-    write-host -ForegroundColor Green "Publishing bundle... $bundle_dir $bundlev"
-    wasm-pack publish -t bundler --tag bundle $bundle_dir
+    # write-host -ForegroundColor Green "Publishing web... $web_dir $webv"
+    # wasm-pack publish -t web --tag web $web_dir
+    # write-host -ForegroundColor Green "Publishing nodejs... $nodejs_dir $njsv"
+    # wasm-pack publish -t nodejs --tag nodejs   $nodejs_dir
+    # write-host -ForegroundColor Green "Publishing bundle... $bundle_dir $bundlev"
+    # wasm-pack publish -t bundler --tag bundle $bundle_dir
     if($publishLatestTag) {
-        write-host -ForegroundColor Green "note: you need to publish @latest manually"
+        write-host -ForegroundColor Green "Publishing latest... $latest_dir $latestlev"
+        wasm-pack publish -t nodejs --tag latest $latest_dir
+        
     }
 }
